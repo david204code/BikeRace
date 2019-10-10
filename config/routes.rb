03adc1e382e366fa8efa81650f9870cdfa49845d
David Wu <david204code@gmail.com>
2019-10-11 00:26:03 +0100

    rails generate scaffold_controller Contest and added resources :contests onto the routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get 'page/home'
   get 'page/photo'
   get 'page/location'
+
+  resources :contests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
