91e5e931a40a2bfb51af706af31a30402d329661
David Wu <david204code@gmail.com>
2019-10-10 21:47:01 +0100

    At the routes.rb, changed the root to 'page#home'

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  
+  root 'page#home'
+  
   get 'page/home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
