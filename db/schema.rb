fc9a587eef1a84fc799cce80426341aaa81954d4
David Wu <david204code@gmail.com>
2019-10-11 00:31:37 +0100

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_10_231602) do
+ActiveRecord::Schema.define(version: 2019_10_10_232909) do
 
   create_table "contests", force: :cascade do |t|
     t.string "firstname"
     t.string "lastname"
     t.string "email"
     t.string "slogan"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "riders", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.string "city_of_origin"
+    t.string "state_of_origin"
+    t.float "latitude"
+    t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
