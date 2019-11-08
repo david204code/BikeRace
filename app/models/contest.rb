a63c8162edfcb7330e928b2de3a4f94d0ff722de
David Wu <david204code@gmail.com>
2019-11-08 20:54:07 +0000

    On the contest model added vlidates: length to maximum 50

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -2,5 +2,6 @@ class Contest < ApplicationRecord
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :email, presence: true
-  validates :slogan, presence: true
+  validates :slogan, presence: true,
+                      length: { maximum: 50}
 end
