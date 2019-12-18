755920e98e9a9b6a739dae19d0a48219446f88a8
David Wu <david204code@gmail.com>
2019-12-18 23:09:08 +0000

    Wrote the test to validate and check the present of the contest form

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,7 +1,7 @@
 class Contest < ApplicationRecord
   validates :firstname, presence: true
   validates :lastname, presence: true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX }
   validates :slogan, presence: true,
--- test/models/contest_test.rb
+++ test/models/contest_test.rb
@@ -4,4 +4,32 @@ class ContestTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @contest = Contest.new(firstname: "First", lastname: "Second", email: "firstsecond@example.com", slogan: "I am going to win")
+  end
+
+  test "filled in correctly" do
+    assert @contest.valid?
+  end
+
+  test "firstname present" do
+    @contest.firstname = " "
+    assert_not @contest.valid?
+  end
+
+  test "lastname present" do
+    @contest.lastname = " "
+    assert_not @contest.valid?
+  end
+
+  test "email present" do
+    @contest.email = " "
+    assert_not @contest.valid?
+  end
+
+  test "slogan present" do 
+    @contest.slogan = " "
+    assert_not @contest.valid?
+  end
+
 end
