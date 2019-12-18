81560de9c649bfb5f7182dd41b5173673937fa9e
David Wu <david204code@gmail.com>
2019-12-18 19:00:01 +0000

    Email Validation with email regex on the contest model

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,7 +1,9 @@
 class Contest < ApplicationRecord
   validates :firstname, presence: true
   validates :lastname, presence: true
-  validates :email, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }
   validates :slogan, presence: true,
                       length: { maximum: 50}
 end
