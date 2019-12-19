4236bf8760cd1f6de982c957bdbf54032f61182b
David Wu <david204code@gmail.com>
2019-12-19 23:47:31 +0000

    On the test integration, wrote test on assert_template and assert_select

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,4 +4,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "header links" do
+    get root_path
+    assert_template 'layouts/_header'
+    assert_select "a[href=?]", home_path, count:2
+    assert_select "a[href=?]", photo_path
+    assert_select "a[href=?]", location_path
+    assert_select "a[href=?]", riders_path
+    assert_select "a[href=?]", contests_path
+  end
+
 end
