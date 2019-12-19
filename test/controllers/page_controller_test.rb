a251b1786e8dcf028673067a9705b0b584b86e60
David Wu <david204code@gmail.com>
2019-12-19 22:41:58 +0000

    Wrote the assert_select test for the title of the page and added get home_path test

--- test/controllers/page_controller_test.rb
+++ test/controllers/page_controller_test.rb
@@ -1,19 +1,33 @@
 require 'test_helper'
 
 class PageControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @full_title = "Boulder Bike Tour"
+  end
+  
   test "should get root" do
     get '/'
     assert_response :success
+    assert_select "title", "Home - #{@full_title}"
+  end
+
+  test "should get home" do
+    get home_path
+    assert_response :success
+    assert_select "title", "Home - #{@full_title}"
   end
 
   test "should get photo" do 
     get photo_path
     assert_response :success
+    assert_select "title", "Photo - #{@full_title}"
   end
 
   test "should get location" do
     get location_path
     assert_response :success
+    assert_select "title", "Location - #{@full_title}"
   end
 
 end
